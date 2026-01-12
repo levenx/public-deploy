d8c60df22baf90e531eb8895e0cd8900aedfaa0b
sunlejin <sunlejin@bytedance.com>
2026-01-12 14:59:56 +0800

    feat: auto deploy latest build on 2026-01-12 14:59:49

--- material-module/@mf-types/compiled-types/components/badge-ribbon/index.d.ts
+++ material-module/@mf-types/compiled-types/components/badge-ribbon/index.d.ts
@@ -2,5 +2,5 @@ import type { RibbonProps } from "antd/es/badge/Ribbon";
 interface BadgeRibbonProps extends RibbonProps {
     visible?: boolean;
 }
-export declare function BadgeRibbon(props: BadgeRibbonProps): string | number | boolean | import("react/jsx-runtime").JSX.Element | Iterable<import("react").ReactNode> | null | undefined;
+export declare function BadgeRibbon(props: BadgeRibbonProps): string | number | bigint | boolean | import("react/jsx-runtime").JSX.Element | Iterable<import("react").ReactNode> | Promise<string | number | bigint | boolean | import("react").ReactPortal | import("react").ReactElement<unknown, string | import("react").JSXElementConstructor<any>> | Iterable<import("react").ReactNode> | null | undefined> | null | undefined;
 export {};
