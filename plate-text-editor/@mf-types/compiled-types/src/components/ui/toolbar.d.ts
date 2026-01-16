4b051da7c6036ed0eb82fd0fce78bb9667d740c6
sunlejin <sunlejin@bytedance.com>
2026-01-16 22:01:01 +0800

    feat: auto deploy latest build on 2026-01-16 22:00:43

--- plate-text-editor/@mf-types/compiled-types/src/components/ui/toolbar.d.ts
+++ plate-text-editor/@mf-types/compiled-types/src/components/ui/toolbar.d.ts
@@ -1,9 +1,9 @@
-import * as ToolbarPrimitive from '@radix-ui/react-toolbar';
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
-import { type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
-import { DropdownMenuRadioGroup } from '@/components/ui/dropdown-menu';
-import { Tooltip, TooltipTrigger } from '@/components/ui/tooltip';
+import * as ToolbarPrimitive from "@radix-ui/react-toolbar";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { type VariantProps } from "class-variance-authority";
+import * as React from "react";
+import { DropdownMenuRadioGroup } from "@/components/ui/dropdown-menu";
+import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
 export declare function Toolbar({ className, ...props }: React.ComponentProps<typeof ToolbarPrimitive.Root>): import("react/jsx-runtime").JSX.Element;
 export declare function ToolbarToggleGroup({ className, ...props }: React.ComponentProps<typeof ToolbarPrimitive.ToolbarToggleGroup>): import("react/jsx-runtime").JSX.Element;
 export declare function ToolbarLink({ className, ...props }: React.ComponentProps<typeof ToolbarPrimitive.Link>): import("react/jsx-runtime").JSX.Element;
@@ -19,18 +19,18 @@ declare const dropdownArrowVariants: (props?: ({
 type ToolbarButtonProps = {
     isDropdown?: boolean;
     pressed?: boolean;
-} & Omit<React.ComponentPropsWithoutRef<typeof ToolbarToggleItem>, 'asChild' | 'value'> & VariantProps<typeof toolbarButtonVariants>;
+} & Omit<React.ComponentPropsWithoutRef<typeof ToolbarToggleItem>, "asChild" | "value"> & VariantProps<typeof toolbarButtonVariants>;
 export declare const ToolbarButton: ({ tooltip, tooltipContentProps, tooltipProps, tooltipTriggerProps, ...props }: TooltipProps<({ children, className, isDropdown, pressed, size, variant, ...props }: ToolbarButtonProps) => import("react/jsx-runtime").JSX.Element>) => import("react/jsx-runtime").JSX.Element;
 export declare function ToolbarSplitButton({ className, ...props }: React.ComponentPropsWithoutRef<typeof ToolbarButton>): import("react/jsx-runtime").JSX.Element;
-type ToolbarSplitButtonPrimaryProps = Omit<React.ComponentPropsWithoutRef<typeof ToolbarToggleItem>, 'value'> & VariantProps<typeof toolbarButtonVariants>;
+type ToolbarSplitButtonPrimaryProps = Omit<React.ComponentPropsWithoutRef<typeof ToolbarToggleItem>, "value"> & VariantProps<typeof toolbarButtonVariants>;
 export declare function ToolbarSplitButtonPrimary({ children, className, size, variant, ...props }: ToolbarSplitButtonPrimaryProps): import("react/jsx-runtime").JSX.Element;
-export declare function ToolbarSplitButtonSecondary({ className, size, variant, ...props }: React.ComponentPropsWithoutRef<'span'> & VariantProps<typeof dropdownArrowVariants>): import("react/jsx-runtime").JSX.Element;
+export declare function ToolbarSplitButtonSecondary({ className, size, variant, ...props }: React.ComponentPropsWithoutRef<"span"> & VariantProps<typeof dropdownArrowVariants>): import("react/jsx-runtime").JSX.Element;
 export declare function ToolbarToggleItem({ className, size, variant, ...props }: React.ComponentProps<typeof ToolbarPrimitive.ToggleItem> & VariantProps<typeof toolbarButtonVariants>): import("react/jsx-runtime").JSX.Element;
-export declare function ToolbarGroup({ children, className, }: React.ComponentProps<'div'>): import("react/jsx-runtime").JSX.Element;
+export declare function ToolbarGroup({ children, className, }: React.ComponentProps<"div">): import("react/jsx-runtime").JSX.Element;
 type TooltipProps<T extends React.ElementType> = {
     tooltip?: React.ReactNode;
-    tooltipContentProps?: Omit<React.ComponentPropsWithoutRef<typeof TooltipContent>, 'children'>;
-    tooltipProps?: Omit<React.ComponentPropsWithoutRef<typeof Tooltip>, 'children'>;
+    tooltipContentProps?: Omit<React.ComponentPropsWithoutRef<typeof TooltipContent>, "children">;
+    tooltipProps?: Omit<React.ComponentPropsWithoutRef<typeof Tooltip>, "children">;
     tooltipTriggerProps?: React.ComponentPropsWithoutRef<typeof TooltipTrigger>;
 } & React.ComponentProps<T>;
 declare function TooltipContent({ children, className, sideOffset, ...props }: React.ComponentProps<typeof TooltipPrimitive.Content>): import("react/jsx-runtime").JSX.Element;
